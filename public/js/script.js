e41235f8f2b2b331acbbd2b3572e53e33b25740e
Braao7 <112507843+Braao7@users.noreply.github.com>
2024-03-12 10:53:00 -0300

    Add files via upload

--- public/js/script.js
+++ public/js/script.js
@@ -48,17 +48,13 @@ function fecharModal() {
     modal.style.display = 'none';
     reiniciarTutorial();
 }
-function passoAnterior() {
-    // Avança para o próximo passo
-    passoAtual--;
-    atualizarConteudo();
-   
-}
 
 function proximoPasso() {
     // Avança para o próximo passo
     passoAtual++;
     atualizarConteudo();
+
+
     if (passoAtual > 5) {  // no momento temos 5 passos então caso seja adicionado algum esse número tem que aumentar
         fecharModal();
     }
@@ -70,38 +66,28 @@ function atualizarConteudo() {
         case 1:
            conteudoModal.innerHTML = "<h2>Foto de perfil:</h2><p>Aqui você pode adicionar sua foto de perfil clicando sobre o avatar e selecionando uma de sua preferência</p>";
            var imagemModal = document.createElement('img');
-           imagemModal.src = 'public/assets/perfil.png'; 
+           imagemModal.src = 'public/assets/user-icon.png'; 
            conteudoModal.appendChild(imagemModal);
            break;
         case 2:
             conteudoModal.innerHTML = "<h2>Ordenar:</h2><p>Aqui você pode ordenar seus prontuários entre os mais antigos e mais atuais para facilitar a busca</p>";
             var imagemModal = document.createElement('img');
-           imagemModal.src = 'public/assets/pii.png'; 
+           imagemModal.src = 'fa-solid fa-arrow-down'; 
            conteudoModal.appendChild(imagemModal);
             break;
         case 3:
             conteudoModal.innerHTML = "<h2>Menu:</h2><p>O menu te permite ter acesso a outras funcionalidades da plataforma</p>";
-            var imagemModal = document.createElement('img');
-           imagemModal.src = 'public/assets/menu.png'; 
-           conteudoModal.appendChild(imagemModal);
             break;
             case 4:
                 conteudoModal.innerHTML = "<h2>Configurações</h2><p> ...</p>";
-                var imagemModal = document.createElement('img');
-           imagemModal.src = 'public/assets/configuracoes.png'; 
-           conteudoModal.appendChild(imagemModal);
                 
                 break;
                 case 5:
                     conteudoModal.innerHTML = "<h2>Acesso aos prontuários</h2><p> ...</p>";
-                    var imagemModal = document.createElement('img');
-           imagemModal.src = 'public/assets/pii.png'; 
-           conteudoModal.appendChild(imagemModal);
                     break;
              
 
     }
-    
 }
 
    // Reinicia para o modal 1 caso o usuário feche
